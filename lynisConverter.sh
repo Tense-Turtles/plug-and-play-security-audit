08ac05fdb18011ad97e5199d29cfd7aa53251905
Beast-Sanjay <sanjuniky2002@gmail.com>
2023-03-31 12:17:15 +0530

    Lynis convertor added

---
+++ lynisConverter.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "Welcome to Plug and Play Audit System"
+
+# converter package required 
+git clone https://github.com/d4t4king/lynis-report-converter.git
+cd lilynis-report-converter
+
+
+# Dependencies for converting raw data to pdf 
+
+apt -y install htmldoc libxml-writer-perl libarchive-zip-perl libjson-perl
+pushd /tmp/
+wget http://search.cpan.org/CPAN/authors/id/M/MF/MFRANKL/HTML-HTMLDoc-0.10.tar.gz
+tar xvf HTML-HTMLDoc-0.10.tar.gz
+pushd HTML-HTMLDoc-0.10
+perl Makefile.PL
+make && make install
+popd
+wget http://search.cpan.org/CPAN/authors/id/J/JM/JMCNAMARA/Excel-Writer-XLSX-0.95.tar.gz
+tar xvf Excel-Writer-XLSX-0.95.tar.gz
+pushd Excel-Writer-XLSX-0.95
+perl Makefile.PL
+make && make install
+popd
+popd
+
+# Execution command
+
+./lynis-report-converter.pl -j -o json_report.json
+./lynis-report-converter.pl -p -o report.pdf
+
+exit 0
