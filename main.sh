40c3fd53afaec25e93ac069ece8d3d824323b502
K S Shailesh <ksshailesh05@gmail.com>
2023-03-31 11:57:34 +0530

    factorization

--- main.sh
+++ main.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
-# Simple menu system that reacts to screen size
-
-# Simple script that gets root privileges at the start
-
 # Check if the script is being run as root
 if [ "$EUID" -ne 0 ]; then
-    # If not, use sudo to re-run the script as root
     sudo "$0" "$@"
     exit $?
 fi
-
-# The rest of the script goes here
 
 # Get the current screen size
 screen_size=$(stty size)
@@ -22,7 +15,6 @@ cols=$(echo $screen_size | cut -d' ' -f2)
 # Define the menu options
 options=("Option 1" "Option 2" "Option 3" "Quit")
 
-# Function to display the menu
 show_menu () {
     clear
     echo "Please choose an option:"
@@ -31,14 +23,12 @@ show_menu () {
     done
 }
 
-# Function to get user input
 get_input () {
     local choice
     read -p "Enter your choice [1-${#options[@]}]: " choice
     echo $choice
 }
 
-# Main loop
 while true; do
     show_menu
     choice=$(get_input)
@@ -53,7 +43,3 @@ while true; do
 done
 
 clear
-
-# This script uses the `stty` command to get the current screen size and then extracts the number of rows and columns. It defines a set of menu options and two functions: one to display the menu and another to get user input. The main loop displays the menu, gets user input, and reacts accordingly. When the user chooses to quit, the loop exits and the script clears the screen.
-
-# I hope this helps! Let me know if you have any questions or need further assistance.
